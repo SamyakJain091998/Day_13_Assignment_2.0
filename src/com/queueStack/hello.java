ea7bceb26d1d26b915044e499ddb1ede08d0a1b3
Samyak Jain <32704791+SamyakJain091998@users.noreply.github.com>
2020-10-08 11:46:58 +0530

    Modified: hello.java updated

--- src/com/queueStack/hello.java
+++ src/com/queueStack/hello.java
@@ -4,7 +4,7 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello! This is main branch.");
+		System.out.println("Hello! This is branch 1.");
 	}
 
 }
